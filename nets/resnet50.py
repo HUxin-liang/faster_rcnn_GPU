bb3ec7a4efc214fd67c385ff59bba891fa3ee245
huxinliang <huxinliang2019@ia.ac.cn>
2020-08-25 16:53:28 +0800

    add utils

--- nets/resnet50.py
+++ nets/resnet50.py
@@ -5,3 +5,133 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import math
+
+model_urls = {
+    'resnet18' : '../resnet_pth/resnet18-5c106cde.pth',
+    'resnet34' : '../resnet_pth/resnet34-333f7ec4.pth',
+    'resnet50' : '../resnet_pth/resnet50-19c8e357.pth',
+    'resnet101' : '../resnet_pth/resnet101-5d3b4d8f.pth',
+    'resnet152' : '../resnet_pth/resnet152-b121ed2d.pth'
+}
+
+class Bottleneck(nn.Module):
+    '''
+    conv-block or identity-block
+    '''
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride = 1, downsample = None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes,kernel_size=3,stride=stride,bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,stride=1,padding=1,bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes*4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes*4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def foward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_class=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0, ceil_mode=True)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AvgPool2d(7)
+        self.fc = nn.Linear(512 * block.expansion)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+    def _make_layer(self, block, planes, blocks_count, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride,bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks_count):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def resnet50():
+    # [3,4,6,3]
+    # 第一层：3=1+2，1个conv block,2个indentity block
+    # 第二层：4=1+3
+    # 第三层：6=1+5
+    model = ResNet(Bottleneck, [3, 4, 6, 3])
+
+    # 获得特征
+    features = list([model.conv1,
+                     model.bn1,
+                     model.relu,
+                     model.maxpool,
+                     model.layer1,
+                     model.layer2,
+                     model.layer3])
+    # 获取分类部分
+    classifier = list([model.layer4, model.avgpool])
+
+    features = nn.Sequential(*features)
+    classifier = nn.Sequential(*classifier)
+
+    return features, classifier
---
+++ utils/utils.py
@@ -0,0 +1,77 @@
+import torch
+import numpy as np
+from torch.nn import functional as F
+
+def bbox2loc(src_bbox, dst_bbox):
+    width = src_bbox[:, 2] - src_bbox[:, 0]
+    height = src_bbox[:, 3] - src_bbox[:, 1]
+    ctr_x = src_bbox[:, 0] + 0.5 * width
+    ctr_y = src_bbox[:, 1] + 0.5 * height
+
+    base_width = dst_bbox[:, 2] - dst_bbox[:, 0]
+    base_height = dst_bbox[:, 3] - dst_bbox[:, 1]
+    base_ctr_x = dst_bbox[:, 0] + 0.5 * base_width
+    base_ctr_y = dst_bbox[:, 1] + 0.5 * base_height
+
+    eps = np.finfo(height.dtype).eps
+    width = np.maximum(width, eps)
+    height = np.maximum(height, eps)
+
+    dx = (base_ctr_x - ctr_x) / width
+    dy = (base_ctr_y - ctr_y) / height
+    dw = np.log(base_width / width)
+    dh = np.log(base_height / height)
+
+    loc = np.vstack((dx, dy, dw, dh)).transpose()
+    return loc
+
+def loc2bbox(src_bbox, loc):
+    if src_bbox.shape[0] == 0:
+        return np.zeros((0, 4), dtype=loc.dtype)
+
+    src_bbox = src_bbox.astype(src_bbox.dtype, copy=False)
+    src_width = src_bbox[:, 2] - src_bbox[:, 0]
+    src_height = src_bbox[:, 3] - src_bbox[:, 1]
+    src_ctr_x = src_bbox[:, 0] + 0.5 * src_width
+    src_ctr_y = src_bbox[:, 1] + 0.5 * src_height
+
+    dx = loc[:, 0::4]
+    dy = loc[:, 1::4]
+    dw = loc[:, 2::4]
+    dh = loc[:, 3::4]
+
+    ctr_x = dx * src_width[:, np.newaxis] + src_ctr_x[:, np.newaxis]
+    ctr_y = dy * src_height[:, np.newaxis] + src_ctr_y[:, np.newaxis]
+    w = np.exp(dw) * src_width[:, np.newaxis]
+    h = np.exp(dh) * src_height[:, np.newaxis]
+
+    dst_bbox = np.zeros(loc.shape, dtype=loc.dtype)
+    dst_bbox[:, 0::4] = ctr_x - 0.5 * w
+    dst_bbox[:, 1::4] = ctr_y - 0.5 * h
+    dst_bbox[:, 2::4] = ctr_x + 0.5 * w
+    dst_bbox[:, 3::4] = ctr_y + 0.5 * h
+
+    return dst_bbox
+
+def nms(detections_class, nms_thres=0.7):
+    max_detections = []
+    while np.shape(detections_class)[0]:
+        # 取出这一类置信度最高的
+        max_detections.append(np.expand_dims(detections_class[0],0))
+        if len(detections_class) == 1:
+            break
+        ious =
+
+
+def bbox_iou(bbox_a, bbox_b):
+    if bbox_a.shape[1] !=4 or bbox_b.shape[1] !=4:
+        print(bbox_a, bbox_b)
+        raise IndexError
+    # top left
+    tl = np.maximum(bbox_a[:, None, :2], bbox_b[:, :2])
+    # bottom right
+    br = np.minimum(bbox_a[:, None, 2:], bbox_b[:, 2:])
+    area_i = np.prod(br - tl, axis=2) * (tl < br).all(axis=2)
+    area_a = np.prod(bbox_a[:, 2:] - bbox_a[:, :2], axis=1)
+    area_b = np.prod(bbox_b[:, 2:] - bbox_b[:, :2], axis=1)
+    return area_i / (area_a[:, None] + area_b - area_i)
